a656f7292a521f33eae6c65eec4df67453ed1525
Mag <hicugi@gmail.com>
2019-08-14 06:45:43 +0600

    header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import React from 'react';
 function App() {
   return (
     <main className="app-main">
-      <div className="block"></div>
+      <section className="block">
+        <header className="block__header">
+          <h1 className="block__title">Билет 1</h1>
+          <button className="icon-magic"></button>
+        </header>
+
+        <div className="block__body">{/*  */}</div>
+      </section>
     </main>
   );
 }
